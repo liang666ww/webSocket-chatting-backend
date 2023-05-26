03082142579dc82313f1d395f6de152224938333
贺政涛 <hzt627471151@163.com>
2023-05-26 13:00:32 +0800

    重新设置登录模块

--- src/main/kotlin/top/zfxt/chat/controller/LoginController.kt
+++ src/main/kotlin/top/zfxt/chat/controller/LoginController.kt
@@ -18,16 +18,17 @@ class LoginController {
     @GetMapping("/login")
     public fun login(username:String): String? {
         //第一次注册用户
-        if(username!="")
+        if(username!="" && UsersMap.get(username)==null)
         {
             //生成用户信息
             var userId = UUID.randomUUID().toString().replace("-","").substring(0,10)
             UsersMap.put(username,User(userId,username,null,"offline"))
 
             println(UsersMap)
-            return JSONUtil.toJsonStr(UsersMap)
+//            return JSONUtil.toJsonStr(UsersMap)
+            return "ok"
         }
-            return null
+            return "repeat"
 
 
     }
